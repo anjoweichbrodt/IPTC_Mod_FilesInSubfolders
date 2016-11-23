b6d687c08f4abbf3853f5c37f5336e91abc5d54f
Personnel temporaire <ZCUTMP@adacv.ad.etat-de-vaud.ch>
2016-11-23 09:58:51 +0100

    Dependencies added to the project

--- IPTC_Mod_FilesInSubfolders.py
+++ IPTC_Mod_FilesInSubfolders.py
@@ -3,39 +3,44 @@
 import os
 import exiftool
 import time
+import re
+import Tkinter, tkFileDialog
 
+root = Tkinter.Tk()
+root.withdraw()
+dirname=os.path.normpath(tkFileDialog.askdirectory(parent=root,title="Selectionner le dossier")).encode('ascii') # Sans l'encoding ascii ca se passe mal, ca doit venir de l'interfacage avec la lib exiftool
+configFileName=os.path.join(dirname, "exif.txt")
+validConfigLine=re.compile("^([^#].+?)(#|$)") # Afin de supporter les commentaires dans le fichier de config (#)
+
+if not dirname:
+        raise "Il faut choisir un dossier"
+
+if not os.path.exists(configFileName):
+        configFileName=tkFileDialog.askopenfilename(parent=root,title="Selectionner le fichier exif",filetypes=(("All files", "*.*"),("Text files", "*.txt")))
+
+if not configFileName:
+        raise "Il faut choisir un fichier exif"
+
+# Lecture des parametre depuis le fichier de config
+settings=["-overwrite_original"]
+with open(configFileName) as f:
+        for line in f:
+                m=validConfigLine.match(line)
+                if m:
+                        settings.append(m.groups()[0])
 StartClock = time.time()        # Permet de voir le temps mis par le script
 CountDone = 0                   # Permet de compter le nombre de fichiers traités
 ImgPath2Title_Log = open("IPTC-_Log.txt", "w")  # Fichier log enregistrant les fichiers traités
 
 with exiftool.ExifTool() as et:
-	for dirpath, subdirs, files in os.walk(os.getcwd()):    # Boucle sur tous les sous-dossiers
+	for dirpath, subdirs, files in os.walk(dirname):    # Boucle sur tous les sous-dossiers
 		for line in files:                              # Boucle sur tous les fichiers
-			if line.endswith((".jpg", ".JPG", ".jpeg", ".JPEG", ".tif", ".TIF", ".tiff", ".TIFF")): # Seuls ces formats d'image sont pris en considération.
-				ImagePath = os.path.join(dirpath, line)         # Enregistrement du chemin d'accès dans une variable
-				et.execute(                                     # Modifier les données IPTC ici (tout ce qui est en vert avec IDLE
-					"-iptc:ReleaseDate=20130604",
-					"-iptc:ReleaseTime=100006",
-					"-iptc:ExpirationDate=20130604",
-					"-iptc:ExpirationTime=100006",
-					"-iptc:City=Avenches",
-					"-iptc:Province-State=VD",
-					"-iptc:Country-PrimaryLocationCode=CH",
-					"-iptc:Country-PrimaryLocationName=Suisse",
-					"-iptc:headline="+ImagePath,            # Le chemin d'accès au fichier dans le Titre
-					"-iptc:keywords=Tag1",
-					"-iptc:keywords=Tag2",
-					"-iptc:keywords=Tag3",
-					"-iptc:keywords=Tag4",
-					"-iptc:keywords=Tag5",
-					"-iptc:SupplementalCategories=Labo",
-					"-iptc:Credit=Site et Musée Romains d'Avenches",
-					"-iptc:Source=Prise de vue numérique",
-					"-iptc:CopyrightNotice=Copyright © - 2016 - Site et Musée Romains d'Avenches",
-					"-iptc:Contact=musee.romain@vd.ch",
-					ImagePath)                              # Indique quelle image est à modifier. Ne pas toucher!
+			if line.lower().endswith((".jpg", ".jpeg", ".tif", ".tiff")): # Seuls ces formats d'image sont pris en considération.
+				ImagePath = os.path.join(dirpath, line)
+				fileSettings= map(lambda x: x.replace("%ImagePath%",ImagePath),settings)
+				et.execute(*(fileSettings+[ImagePath]))
 				ImgPath2Title_Log.write(ImagePath + "\n")       # Inscription du fichier traité dans le Log
-				CountDone = CountDone + 1			# Une image de plus!
+				CountDone+=1
 				print ImagePath, "DONE"				# Affiche le fichier traité dans la console
 
 
@@ -47,3 +52,4 @@ print TimeLog                                                                   
 
 ImgPath2Title_Log.write(TimeLog)                                                # Inscrire le temps dans le log
 ImgPath2Title_Log.close()
+
